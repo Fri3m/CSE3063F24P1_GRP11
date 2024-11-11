42251ff9ab5943e0913174fbf477395651d9870b
berkan <berkang0331@gmail.com>
2024-11-11 15:11:45 +0300

    transcript

--- src/Transcript.java
+++ src/Transcript.java
@@ -3,21 +3,50 @@ package src;
 import java.util.ArrayList;
 
 public class Transcript {
+
     private Student _student;
     private ArrayList<TakenCourse> _taken_courses;
     private float _GPA;
 
-    Transcript(Student student){
-
+    Transcript(Student student) {
+        this._student = student;
+        this._taken_courses = new ArrayList<>();
+        this._GPA = 0.0f;
     }
 
     public boolean addTakenCourse(TakenCourse tc) {
-        return true;
+        if (tc != null) {
+            _taken_courses.add(tc);
+            calculateGPA();
+            return true;
+        }
+        return false;
     }
 
-    private void calculateGPA(){
+    private void calculateGPA() {
+        if (_taken_courses.isEmpty()) {
+            _GPA = 0.0f;
+            return;
+        }
+        float totalPoints = 0.0f;
+
+        for (TakenCourse course : _taken_courses) {
+            totalPoints += course.getCourseScore().getNumericValue();
+        }
+
+        _GPA = totalPoints / _taken_courses.size();
     }
 
-    //getters
+    // getters
+    public Student getStudent() {
+        return _student;
+    }
+
+    public ArrayList<TakenCourse> getTakenCourses() {
+        return _taken_courses;
+    }
+
+    public float getGPA() {
+        return _GPA;
+    }
 }
-
