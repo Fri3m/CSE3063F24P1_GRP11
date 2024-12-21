f0dc8e77bbe059e68dd434c72889eba4aada7e55
Fri3m <omerisomer@outlook.com>
2024-12-22 00:22:58 +0300

    data management methods

--- pythonCode/DataManagement.py
+++ pythonCode/DataManagement.py
@@ -1,24 +1,33 @@
 import json
 import os
 import random
+from json import JSONEncoder
 
 from fontTools.merge.util import first
 
 from pythonCode.Department import Department, DepartmentID
-from pythonCode.LoginAuthService import UserInformation
+from pythonCode.User import User
+from pythonCode.UserInformation import UserInformation
 from pythonCode.Student import StudentID, Student
 from pythonCode.Transcript import Transcript
 
 
 class DataManagement:
 
-    def saveToJson(self, obj, obj_type):
-        match obj_type:
-            case "Course":
-                self.__saveToJson(obj, f"{obj_type}/{obj.getCourseCode()}.json")
-            case "student":
-                self.__saveToJson(obj,
-                                  f"{obj_type}/1.json")
+    def saveToJson(self, obj):
+        file_name = ""
+        users = ("Student", "Lecturer", "Advisor", "Admin", "DepartmentScheduler", "StudentsAffairs", "DepartmentHead")
+        if type(obj).__name__ in users:
+            file_name = obj.getUserInformation().get_UNIVERSITY_EMAIL()
+        elif type(obj).__name__ == "Department":
+            file_name = obj.getDepartmentID().getDepartmentName()
+        elif type(obj).__name__ == "Faculty":
+            file_name = obj.getFacultyID().getFacultyName()
+        elif type(obj).__name__ == "Course":
+            file_name = obj.getCourseID().getCourseName()
+
+        file_path = f"../{type(obj).__name__}s/{file_name}.json"
+        self.__saveToJson(obj, file_path)
 
     def __saveToJson(self, obj, file_path_and_name):
         # Extract the directory path from the file path
@@ -30,12 +39,146 @@ class DataManagement:
 
         # Open the file and write JSON data
         with open(file_path_and_name, "w") as file:
-            json.dump(obj, file, indent=4)
+            json.dump(obj, file, indent=4, cls=customEncoder)
 
-    def loadFromJson(self, name, obj_type):
+    def loadFromJson(self, file_path_and_name):
+        # Extract the directory path from the file path
+        directory = os.path.dirname(file_path_and_name)
+
+        # Return None if the directory doesn't exist
+        if directory and not os.path.exists(directory):
+            return None
+
+        # Open the file and read JSON data
+        with open(file_path_and_name, "r") as file:
+            return json.load(file)
         pass
 
-    # helper methods and variables for generating random objects
+    def deleteJsonFile(self, file_path_and_name):
+        # Extract the directory path from the file path
+        directory = os.path.dirname(file_path_and_name)
+
+        # Return None if the directory doesn't exist
+        if directory and not os.path.exists(directory):
+            return
+
+        os.remove(file_path_and_name)
+
+    def getAllJsons(self, folder_path):
+        allObjects = []
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                if file.endswith(".json"):
+                    allObjects.append(self.loadFromJson(os.path.join(root, file)))
+        return allObjects
+
+    def getAllStudents(self):
+        return self.getAllJsons("../Students")
+
+    def createOrChangeStudent(self, student):
+        self.saveToJson(student)
+
+    def getStudent(self, university_email):
+        return self.loadFromJson(f"../Students/{university_email}.json")
+
+    def removeStudent(self, university_email):
+        self.deleteJsonFile(f"../Students/{university_email}.json")
+
+    def getAllLecturers(self):
+        return self.getAllJsons("../Lecturers")
+
+    def createOrChangeLecturer(self, lecturer):
+        self.saveToJson(lecturer)
+
+    def getLecturer(self, university_email):
+        return self.loadFromJson(f"../Lecturers/{university_email}.json")
+
+    def removeLecturer(self, university_email):
+        self.deleteJsonFile(f"../Lecturers/{university_email}.json")
+
+    def getAllCourses(self):
+        return self.getAllJsons("../Courses")
+
+    def createOrChangeCourse(self, course):
+        self.saveToJson(course)
+
+    def getCourse(self, course_name):
+        return self.loadFromJson(f"../Courses/{course_name}.json")
+
+    def removeCourse(self, course_name):
+        self.deleteJsonFile(f"../Courses/{course_name}.json")
+
+    def getAllAdvisors(self):
+        return self.getAllJsons("../Advisors")
+
+    def createOrChangeAdvisor(self, advisor):
+        self.saveToJson(advisor)
+
+    def getAdvisor(self, university_email):
+        return self.loadFromJson(f"../Advisors/{university_email}.json")
+
+    def removeAdvisor(self, university_email):
+        self.deleteJsonFile(f"../Advisors/{university_email}.json")
+
+    def getAllFaculties(self):
+        return self.getAllJsons("../Faculties")
+
+    def createOrChangeFaculty(self, faculty):
+        self.saveToJson(faculty)
+
+    def getFaculty(self, faculty_name):
+        return self.loadFromJson(f"../Faculties/{faculty_name}.json")
+
+    def removeFaculty(self, faculty_name):
+        self.deleteJsonFile(f"../Faculties/{faculty_name}.json")
+
+    def getAllDepartments(self):
+        return self.getAllJsons("../Departments")
+
+    def createOrChangeDepartment(self, department):
+        self.saveToJson(department)
+
+    def getDepartment(self, department_name):
+        return self.loadFromJson(f"../Departments/{department_name}.json")
+
+    def removeDepartment(self, department_name):
+        self.deleteJsonFile(f"../Departments/{department_name}.json")
+
+    def getAllDepartmentHeads(self):
+        return self.getAllJsons("../DepartmentHeads")
+
+    def createOrChangeDepartmentHead(self, departmentHead):
+        self.saveToJson(departmentHead)
+
+    def getDepartmentHead(self, university_email):
+        return self.loadFromJson(f"../DepartmentHeads/{university_email}.json")
+
+    def removeDepartmentHead(self, university_email):
+        self.deleteJsonFile(f"../DepartmentHeads/{university_email}.json")
+
+    def getAllDepartmentSchedulers(self):
+        return self.getAllJsons("../DepartmentSchedulers")
+
+    def createOrChangeDepartmentScheduler(self, departmentScheduler):
+        self.saveToJson(departmentScheduler)
+
+    def getDepartmentScheduler(self, university_email):
+        return self.loadFromJson(f"../DepartmentSchedulers/{university_email}.json")
+
+    def removeDepartmentScheduler(self, university_email):
+        self.deleteJsonFile(f"../DepartmentSchedulers/{university_email}.json")
+
+    def getAllStudentsAffairs(self):
+        return self.getAllJsons("../StudentsAffairs")
+
+    def createOrChangeStudentsAffairs(self, studentsAffairs):
+        self.saveToJson(studentsAffairs)
+
+    def getStudentsAffairs(self, university_email):
+        return self.loadFromJson(f"../StudentsAffairs/{university_email}.json")
+
+    def removeStudentsAffairs(self, university_email):
+        self.deleteJsonFile(f"../StudentsAffairs/{university_email}.json")
 
 
 names = ["Ali", "Fatma", "Zeynep", "Ahmet", "Ayse", "Mehmet", "Emine", "Hasan", "Huseyin", "Elif", "Mustafa",
@@ -103,8 +246,7 @@ def __generateRandomUserInformation():
     email = __generateEmail(firstName, lastName)
     universityEmail = __generateUniversityEmail(firstName, lastName)
     password = __generatePassword()
-    ui = UserInformation(encoded_password=password,
-                         university_email=universityEmail)
+    ui = UserInformation(firstName, lastName, password, universityEmail, email, city, phoneNumber)
     return ui
 
 
@@ -115,8 +257,15 @@ def generateRandomStudent(department, entranceDate, entranceRank, advisorID):
     return Student(userInformation, studentID, transcript, advisorID, 2025 - entranceDate)
 
 
-dep = Department(DepartmentID(150,"comp sci"), 2)
+class customEncoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
 
-xx = generateRandomStudent(dep, 2019, 100, 1)
-x = DataManagement()
-x.saveToJson(xx, "student")
+
+dm = DataManagement()
+
+# st = generateRandomStudent(Department(DepartmentID("1", "Computer Engineering"), "1"), 2019, 1, "1")
+# dm.saveToJson(st)
+
+students = dm.getAllStudents()
+print(students)
