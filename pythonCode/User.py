a582a03a651153b7ed6b22a3ee73de6528ad0c1f
yusuf <peksamet1@gmail.com>
2024-12-21 17:21:30 +0300

    python

--- pythonCode/User.py
+++ pythonCode/User.py
@@ -12,33 +12,33 @@ class Staff(User):
         self._staffId = StaffId
 
     def get_staffId(self):
-        return self._staff_id
+        return self._staffId
 
 class StaffId():
     _staffIdCounter = 1
 
     def __init__(self):
-        _staffId = _generateStaffId()
+        _staffId = self._generateStaffId()
 
     @staticmethod
-    def _generateStaffId(self):
-        self._staffId = _staffIdCounter
-        staffId._staffIdCounter += 1
-        return self._staffId
+    def _generateStaffId():
+        _staffId = StaffId._staffIdCounter
+        StaffId._staffIdCounter += 1
+        return _staffId
 
     def get_staff_id(self):
-        return self._staff_id
+        return self._staffId
 
     @staticmethod
-    def changeStaticCounter(self, newCounter):
-        if newCounter > _staffIdCounter:
-            _staffIdCounter = newCounter
+    def changeStaticCounter( newCounter):
+        if newCounter > StaffId._staffIdCounter:
+            StaffId._staffIdCounter = newCounter
 
 class Lecturer(Staff):
     def __init__(self, user_information):
         super().__init__(user_information)
-        self._departmentId = DepartmentId
-        self._facultyId = FacultyId
+        self._departmentId = ""
+        self._facultyId = ""
 
     def get_departmentId(self):
         return self._departmentId
@@ -50,7 +50,8 @@ class Advisor(Lecturer):
         pre = courseRequest.get_course().getCourseRequirements()
         return pre.pre.isStudentQualified(courseRequest.get_student())
     def approveCourseRequest(self, courseRequest):
-        ccourseRequest.get_student().get_current_courses().add(courseRequest.get_course())
+        courseRequest.get_student().get_current_courses().add(courseRequest.get_course())
+
 
 class Admin(Staff):
     def __init__(self, user_information):
@@ -63,7 +64,7 @@ class DepartmentScheduler(Staff):
         departmentCourses = []
         for course in courses:
             if course.getCourseRequirements().get_departmentID().getDepartmentName().equalsIgnoreCase(department.getDepartmentID().getDepartmentName()):
-                departmentCourses.add(course)
+                departmentCourses.append(course)
         return departmentCourses
     def changeCourseSectionDayAndTime(self, courseSection, day, time):
         courseSection._day = day
